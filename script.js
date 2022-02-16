f48874d968a600f49e20f7ac92f8c41e89b81048
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-02-16 05:07:57 -0300

    inicializaçao js, abrindo e voltando tela 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-
-
-
 function abrirQuizz(clique){
-    
+    const esconderTela1 = document.querySelector(".tela1");
+    const abrirTela2 = document.querySelector(".tela2");
+    esconderTela1.classList.add("escondido");
+    abrirTela2.classList.remove("escondido");
 }
-
+function voltarPraHome(){
+    const esconderTela2 = document.querySelector(".tela2");
+    const abrirTela1 = document.querySelector(".tela1");
+    esconderTela2.classList.add("escondido");
+    abrirTela1.classList.remove("escondido");
+}
